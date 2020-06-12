b904a44d87dbcd9210652eb997eb58add8a1ffe1
Leandro Toloza <leandrotoloza8@gmail.com>
2020-06-12 13:23:40 -0300

    comenzando con Json(generico), registro de usuarios

---
+++ TPlaboratorioIII/src/com/company/archives/JsonFunctions.java
@@ -0,0 +1,32 @@
+package com.company.archives;
+import com.company.user.User;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+/*
+
+public class JsonFunctions<G> {
+    private Class<G> gClass;
+
+    public List<G> readFile(Class className){
+        String fileName = "archives\\";
+
+        if (className.getName().equals(User.class.getName())){
+            fileName += "Users.json";
+        }//CON IF ELSE E IMPLEMENTANDO LO MISMO, PUEDEN CREARSE DISTINTOS ARCHIVOS (GENERICIDAD PAPA)
+
+        File file = new File(fileName);
+        List<G> readFileElements = null;
+
+        if (file.exists()){
+            //InputStreamReader;
+        }else
+            return null;
+    }
+
+
+
+}
+*/
--- TPlaboratorioIII/src/com/company/user/User.java
+++ TPlaboratorioIII/src/com/company/user/User.java
@@ -1,27 +1,131 @@
 package com.company.user;
-import java.util.Scanner;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
 
 public class User {
+
 
     private String name;
     private String surName;
     private int dni;
     private int age;
+    private UUID id;
+    private String password;
 
+    public User(){}
 
-
-
-    public void registerUser(){
-            Scanner in = new Scanner (System.in);
-            System.out.println("Enter Your Name: \n");
-            this.name = in.nextLine();
-            System.out.println("Enter Your SurName: \n");
-            this.surName = in.nextLine();
-            System.out.println("Enter Your DNI: \n");
-            this.dni = in.nextInt();
-            System.out.println("Enter Your Age: \n");
-            this.age = in.nextInt();
-            //// SAVE ON JSON
+    public User(String name, String surName, int dni, int age, String password) {
+        this.id = UUID.randomUUID();
+        this.name = name;
+        this.surName = surName;
+        this.dni = dni;
+        this.age = age;
+        this.password = password;
     }
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurName() {
+        return surName;
+    }
+
+    public void setSurName(String surName) {
+        this.surName = surName;
+    }
+
+    public int getDni() {
+        return dni;
+    }
+
+    public void setDni(int dni) {
+        this.dni = dni;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "name='" + name + '\'' +
+                ", surName='" + surName + '\'' +
+                ", dni=" + dni +
+                ", age=" + age +
+                ", id=" + id +
+                ", password='" + password + '\'' +
+                '}';
+    }
+
+
+    public List<User> readFile(){
+      //  File myfile = new File(path);
+        return null;
+    }
+
+    public boolean writeFile(User element){
+        File file = new File("archives\\Users.json");
+
+        try{
+            if (file.createNewFile()){ //Crea el archivo si no existe
+                ArrayList<User> usersRead = new ArrayList<>();
+                usersRead.add(element);
+            }else{
+                ArrayList<User> usersRead = new ArrayList<User>(readFile()); //creo usersRead con los usuarios existentes en el archivo
+                usersRead.add(element);
+            }
+            return true;
+        } catch (IOException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    public boolean writeArrayToFile(List<User> elements) {
+        File file = new File("archives\\Users.json");
+
+        try {
+            if(file.createNewFile()){//Crea el archivo si no existe
+                ArrayList<User> usersArrayList = new ArrayList<>();
+                usersArrayList.addAll(elements);
+            }else{
+                ArrayList<User> readedValues = new ArrayList<User>(readFile());
+                readedValues.addAll(elements);
+            }
+            return true;
+        } catch (IOException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
 }
