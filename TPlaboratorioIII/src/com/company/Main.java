f26f06dfa702356463e0454aecf4d93df72feb36
pedro <pedrobazterrica@hotmail.com>
2020-06-14 20:42:28 -0300

    Fui obligado a commitear

--- TPlaboratorioIII/src/com/company/Main.java
+++ TPlaboratorioIII/src/com/company/Main.java
@@ -1,13 +1,19 @@
 package com.company;
 
+import com.company.airplane.Gold;
+import com.company.airplane.MotorType;
+import com.company.tickets.City;
+import com.company.tickets.Ticket;
 import com.company.user.User;
+
+import java.time.LocalDate;
 
 public class Main {
 
     public static void main(String[] args) {
 
 
-
+        /*
         User testUSUARIO = new User("Lean", "Tolo", 111111, 22, "123456");
         User testUSUARIO2 = new User("Lau", "Nieves", 222222, 22, "789456");
         User testUSUARIO3 = new User("Pedro", "Vago", 333333, 22, "123789");
@@ -17,5 +23,11 @@ public class Main {
         testUSUARIO3.addUserToFile(testUSUARIO3);
 
         testUSUARIO3.readFile();
+
+         */
+        Gold avion = new Gold(15,15,5,450, MotorType.HELICE);
+        Ticket ticket = new Ticket(LocalDate.now(), City.MONTEVIDEO,City.SANTIAGO,2,avion);
+        System.out.println(ticket.getDistance());
+        System.out.println(ticket.getPrice());
     }
 }
