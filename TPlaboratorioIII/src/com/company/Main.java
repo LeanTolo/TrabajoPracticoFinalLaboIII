a1025a92b2a057585c2b44eee9f2ddb5441385cf
Leandro Toloza <leandrotoloza8@gmail.com>
2020-06-15 19:41:25 -0300

    se agrega archivado a bronze/silver/etc. Se crea una interfaz json para hacer overray a sus funciones y no declarar nuevas en cada clase

--- TPlaboratorioIII/src/com/company/Main.java
+++ TPlaboratorioIII/src/com/company/Main.java
@@ -1,17 +1,8 @@
 package com.company;
 
-import com.company.airplane.Gold;
-import com.company.airplane.MotorType;
-import com.company.archives.JsonFunctions;
-import com.company.tickets.City;
-import com.company.tickets.Ticket;
 import com.company.user.User;
 
 import java.io.IOException;
-import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.TreeSet;
 
 
 public class Main {
@@ -24,14 +15,14 @@ public class Main {
         User testUSUARIO2 = new User("Lau", "Nieves", 222222, 22, "789456");
         User testUSUARIO3 = new User("Pedro", "Vago", 333333, 22, "123789");
 
-        //testUSUARIO.addUserToFile(testUSUARIO);
-       // testUSUARIO2.addUserToFile(testUSUARIO2);
-      //  testUSUARIO3.addUserToFile(testUSUARIO3);
+        //testUSUARIO.addToFile(testUSUARIO);
+       // testUSUARIO2.addToFile(testUSUARIO2);
+      //  testUSUARIO3.addToFile(testUSUARIO3);
 
 
 
 
-        testUSUARIO3.showFile();
+        //testUSUARIO3.showFile();
 
 /*
         Gold avion = new Gold(15,15,5,450, MotorType.HELICE);
--- TPlaboratorioIII/src/com/company/airplane/Bronze.java
+++ TPlaboratorioIII/src/com/company/airplane/Bronze.java
@@ -1,8 +1,80 @@
 package com.company.airplane;
 
-public class Bronze extends Airplane {
+import com.company.IjsonManagement.IjsonManagement;
+import com.company.user.User;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Bronze extends Airplane implements IjsonManagement<Bronze> {
     public Bronze(double fuelCapacity, double costPerKm, int maxPassengers, double maxVelocity, MotorType motorType) {
         super(fuelCapacity, costPerKm, maxPassengers, maxVelocity, motorType, 3000);
+    }
+
+
+    @Override
+    public List<Bronze> readFile () throws IOException {
+
+        List<Bronze> bronzesFromJson = null;
+        File file = new File("Bronze.json");
+        ObjectMapper mapper = new ObjectMapper();
+
+        if(file.exists()) {
+            try {
+                Bronze[] bronzesArray = mapper.readValue(file,Bronze[].class); // convert JSON array to Array objects
+                bronzesFromJson = Arrays.asList(mapper.readValue(file, Bronze[].class)); // convert JSON array to List of objects
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }else{
+            System.out.println("File is Empty");
+        }
+        return bronzesFromJson;
+    }
+
+    @Override
+    public void addToFile(Bronze element) {
+        File file = new File("Bronze.json");
+        ObjectMapper mapper = new ObjectMapper();
+        try {
+            if (file.createNewFile()) {
+                ArrayList<Bronze> bronzesArrayList = new ArrayList<>();
+                bronzesArrayList.add(element);
+                mapper.writeValue(file, bronzesArrayList);
+                System.out.println("--- Imprimiento en archivo ---\n");
+            } else {
+                ArrayList<Bronze> bronzesArrayList = new ArrayList<Bronze>(readFile());
+                bronzesArrayList.add(element);
+                mapper.writeValue(file, bronzesArrayList);
+            }
+        }catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void showFile () throws IOException {
+        File file = new File("Bronze.json");
+        ObjectMapper mapper = new ObjectMapper();
+
+        if(file.exists()) {
+            System.out.println("--- Contenido del Archivo ---");
+            try {
+
+                Bronze[] bronzesArray = mapper.readValue(file,Bronze[].class); // convert JSON array to Array objects
+                List<Bronze> bronzes = Arrays.asList(mapper.readValue(file, Bronze[].class)); // convert JSON array to List of objects
+                bronzes.stream().forEach(x -> System.out.println(x)); // show lists of objects
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }else{
+            System.out.println("Archivo vacio");
+        }
     }
 
 }
